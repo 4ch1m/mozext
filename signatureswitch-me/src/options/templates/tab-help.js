299dc56177bcc5525779b89c4b6df2ca195e8e05
Achim <git@achimonline.de>
2022-07-02 17:46:45 +0200

    fix link; colorize heart icon

--- signatureswitch-me/src/options/templates/tab-help.js
+++ signatureswitch-me/src/options/templates/tab-help.js
@@ -33,13 +33,13 @@ const HELP_TAB_PANE = `
                 </div>
                 <div class="row mt-2">
                     <div class="col-md-1 text-center">
-                        <svg class="bi" width="2em" height="2em" fill="currentColor">
+                        <svg class="bi" width="2em" height="2em" fill="#ff0000">
                             <use xlink:href="/_images/bootstrap-icons.svg#heart-fill"/>
                         </svg>
                     </div>
                     <div class="col-md-11">
                         <span data-i18n="optionsHelpSupport1"></span>&nbsp;&hellip;<br>
-                        <a href="https://www.achimonline.de/mozext/contact.php" target="_blank">Achim's Mozilla Extensions <small><small>(https://www.achimonline.de/mozext/contact.php)</small></small></a><br>
+                        <a href="https://www.achimonline.de/mozext/contact.php">Achim's Mozilla Extensions <small><small>(https://www.achimonline.de/mozext/contact.php)</small></small></a><br>
                         &hellip;&nbsp;<span data-i18n="optionsHelpSupport2"></span>
                     </div>
                 </div>
